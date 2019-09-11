266206939eb4bbcaf760e57f6dcfb81c256a84e1
github1 <john.collier4@gmail.com>
2019-09-11 12:33:47 -0400

    refactor ajax-service package to use cross-fetch

--- server.mock.js
+++ server.mock.js
@@ -1,8 +1,25 @@
+const retryState = {};
 module.exports = app => {
     app.post('/echo', (req, res) => {
         res.type('application/json');
         res.send(Object.assign({},
             {headers: req.headers},
             {body: req.body}));
+    });
+    app.post('/retry', (req, res) => {
+      if (retryState[req.body.id]) {
+        retryState[req.body.id].attempts++;
+      } else {
+        retryState[req.body.id] = req.body;
+        retryState[req.body.id].attempts = 0;
+      }
+      res.type('application/json');
+      if (retryState[req.body.id].attempts >= retryState[req.body.id].respondIn) {
+        res.status(200);
+        res.send({ status: 'success' });
+      } else {
+        res.status(503);
+        res.send({});
+      }
     });
 };
